dbfda851444f3d5f2fe6e53895e15f0a750c29bb
favourloverboy <favourehio2019@gmail.com>
2022-10-13 02:41:11 +0100

    worked on the 404 page

--- 404.php
+++ 404.php
@@ -6,7 +6,6 @@
     <h1>404</h1>
     <p>this is the 404 page</p>
     <br>
-    <a href="dashboard">Dashboard |</a>
-    <a href="logout.php">Sign In</a>
+    <a href="home">Home</a>
                 
 <?php include('includes/footer.php'); ?>
